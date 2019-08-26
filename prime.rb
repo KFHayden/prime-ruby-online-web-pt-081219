253f8059d37c5f3cd245cda9e81a7657fe469a63
Kevin Hayden <kevin.fhayden@gmail.com>
2019-08-26 00:10:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 def prime?(int)
   if (int <= 3)
     return (int > 1)
-  elsif ((int / 2 == 0) || (int / 3 == 0))
+  elsif ((int % 2 == 0) || (int % 3 == 0))
     return false
   else
     i = 5
     while ((i * i) <= int)
-      if ((int / i == 0) || (int / (i + 2) == 0))
+      if ((int % i == 0) || (int / (i + 2) == 0))
         return false
       end
       i += 1
